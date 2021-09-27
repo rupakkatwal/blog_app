8d52ec21eb62deafd1148a7b429e22718d82ff54
rupakkatwal <rupak2072@gmail.com>
2021-09-27 03:28:44 +0200

    components completed

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navigation from "../components";
+import Header from "../components/Header";
 import IPageProps from "../interfaces/page" ;
 
 const HomePage: React.FunctionComponent<IPageProps> = props => {
     return (
-        <p> Home Page </p>
+        <Container fluid className = "p-0">
+            <Navigation/>
+            <Header
+                title="Coolest Blog Website"
+                headline = "Checkout the blog"
+            />
+            <Container className = "mt-5">
+                View the blog here
+            </Container>
+
+        </Container>
     );
        
 }
